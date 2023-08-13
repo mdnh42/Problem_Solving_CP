9eddc4ebd2f7cfd99d801e8924e31fa0e695716b
itzmrnh <mdnh.42@gmail.com>
2023-08-13 22:32:52 +0600

    Day_03

---
+++ Week_10/Day_03/B_Luntik_and_Subsequences.cpp
@@ -0,0 +1,45 @@
+/*
+itzmrnh
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define ll long long int
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define SIZE 26;
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define sz(v) (int)v.size()
+#define all(v) v.begin(), v.end()
+void solve()
+{
+    int n, x;
+    cin >> n;
+    int cnt0 = 0, cnt1 = 0;
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> x;
+        if (x == 0)
+            cnt0++;
+        if (x == 1)
+            cnt1++;
+    }
+    cout << (1ll << cnt0) * (ll)cnt1 << '\n';
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
